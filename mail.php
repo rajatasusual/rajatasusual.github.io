ea78deeb8c90971a1464c49284be6268c04e8384
rajatasusual <krajat4@gmail.com>
2024-04-28 21:44:41 +0100

    Update till Apr 2024

--- mail.php
+++ mail.php
@@ -1,13 +1,13 @@
 <?php
 
-$recepient = "contact@rajatasusual.online";
+$recepient = "rajat@alumni.ie.edu";
 $sitename = "Rajatasusual";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
 $message = "Name: $name \nEmail: $email \nText: $text";
-$headers = "From: Contact Form <contact@rajatasusual.online>"."\r\n";
+$headers = "From: Contact Form <rajat@alumni.ie.edu>"."\r\n";
 $headers .= "Reply-To:" . $email . "\r\n";
 $headers .= "X-Mailer: PHP/" . phpversion();
 
